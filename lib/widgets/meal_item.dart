9b2c57fffd02858f5b2d35da64281d2c5ff4e88c
Aakash <aakash1350@gmail.com>
2020-07-16 14:30:57 +0530

    Added meals name in mealItem

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,6 +1,20 @@
+import '../models/meal.dart';
 import 'package:flutter/material.dart';
 
 class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.affordability,
+    @required this.complexity,
+  });
   void selectMeal() {}
   @override
   Widget build(BuildContext context) {
@@ -9,6 +23,41 @@ class MealItem extends StatelessWidget {
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(imageUrl),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 250,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
