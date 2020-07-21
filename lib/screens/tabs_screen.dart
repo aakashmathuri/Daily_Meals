0d146022ac4e8b97446d57bb02ff4d25bb5bc854
Aakash <aakash1350@gmail.com>
2020-07-21 20:08:05 +0530

    Removed DefaultTabController

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,7 @@
-import './favorites_screen.dart';
-
-import './categories_screen.dart';
 import 'package:flutter/material.dart';
+
+import './favorites_screen.dart';
+import './categories_screen.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -11,31 +11,27 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      //initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(
-                Icons.category,
-              ),
-              text: 'Category',
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+        bottom: TabBar(tabs: <Widget>[
+          Tab(
+            icon: Icon(
+              Icons.category,
             ),
-            Tab(
-              icon: Icon(Icons.star),
-              text: 'Favorites',
-            ),
-          ]),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+            text: 'Category',
+          ),
+          Tab(
+            icon: Icon(Icons.star),
+            text: 'Favorites',
+          ),
+        ]),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          CategoriesScreen(),
+          FavoritesScreen(),
+        ],
       ),
     );
   }
